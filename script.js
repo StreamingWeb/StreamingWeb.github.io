8458fe30fb2f529fabdd814e1150e5301d4f38ba
morcheysha77 <44336443+morcheysha77@users.noreply.github.com>
2024-06-23 12:52:22 +0200

    Nice

--- script.js
+++ script.js
@@ -1,58 +1,70 @@
 // URL du fichier raw sur GitHub
-const baseUrl = window.location.href.split('/').slice(0, -1).join('/') + '/ressources/';
-const sites = [
-    { url: 'cinehub.wf', flags: ['movie', 'serie', '360p', '720p', '1080p', 'ads'] },
-    { url: 'showboxmovies.net', flags: ['movie', 'serie', '360p', '720p', '1080p', 'ads'] },
-    { url: 'sflix.se', flags: ['movie', 'serie', '360p', '720p', '1080p', 'ads'] },
-    { url: 'bflixhd.to', flags: ['movie', 'serie', '360p', '720p', '1080p', 'ads'] },
-    { url: 'flixflare.to', flags: ['movie', 'serie', '360p', '720p', '1080p', 'ads'] },
-    { url: 'swatchseries.is', flags: ['360p', 'ads'] },
-    { url: 'fmovies.to', flags: ['360p', 'ads'] },
-    { url: 'filmoflix.to', flags: ['360p', 'ads'] },
-    { url: 'movies7.to', flags: ['360p', 'ads'] },
-    { url: 'favmovies.to', flags: ['360p', 'ads'] },
-    { url: 'yeahmovies.to', flags: ['360p', 'ads'] },
-    { url: 'soap2dayx2.to', flags: ['360p', 'ads'] },
-    { url: 'watchseriesx.to', flags: ['movie', 'serie', '360p', '720p', '1080p', 'ads'] },
-    { url: 'sflix.to', flags: ['360p', 'ads'] },
-    { url: 'seriestreaming.in', flags: ['360p', 'ads'] },
-    { url: 'watch4freemovies.com', flags: ['360p', 'ads'] },
-    { url: 'soap2dayhd.co', flags: ['360p', 'ads'] },
-    { url: 'papadustream.bz', flags: ['360p', 'ads'] },
-    { url: 'soap2dayhd.com', flags: ['360p', 'ads'] },
-    { url: 'swatchseries.mx', flags: ['360p', 'ads'] },
-    { url: 'watchmovies.nz', flags: ['360p', 'ads'] },
-    { url: 'soap2day.ist', flags: ['360p', 'ads'] },
-    { url: 'soap2day.pe', flags: ['360p', 'ads'] },
-    { url: '123series.bz', flags: ['360p', 'ads'] },
-    { url: 'soap2day.tf', flags: ['360p', 'ads'] },
-    { url: 'ymovies.cc', flags: ['360p', 'ads'] },
-    { url: 'xmovies.ws', flags: ['360p', 'ads'] },
-    { url: 'watchseries.im', flags: ['360p', 'ads'] },
-    { url: 'watchseries.mn', flags: ['360p', 'ads'] },
-    { url: 'watchseries.pe', flags: ['360p', 'ads'] },
-    { url: 'watchserieshd.mx', flags: ['360p', 'ads'] },
-    { url: 'watchseriesstream.com', flags: ['360p', 'ads'] },
-    { url: '123moviesfree.net', flags: ['360p', 'ads'] },
-    { url: 'f2movies.to', flags: ['360p', 'ads'] },
-    { url: 'vipstream.tv', flags: ['360p', 'ads'] },
-    { url: 'upmovies.net', flags: ['360p', 'ads'] },
-    { url: '1movieshd.cc', flags: ['360p', 'ads'] },
-    { url: '1hd.to', flags: ['360p', 'ads'] },
-    { url: '1flix.to', flags: ['360p', 'ads'] },
-    { url: '123-movies.vc', flags: ['movie', 'serie', '360p', '720p', '1080p', 'ads'] },
-    { url: '123moviestv.net', flags: ['360p', 'ads'] }
+const allSites = [
+    { url: 'cinehub.wf', flags: ['en', 'movie', 'serie', '360p', '720p', '1080p', 'ads', 'web'] },
+    { url: 'showboxmovies.net', flags: ['en', 'movie', 'serie', '360p', '720p', '1080p', 'ads', 'web'] },
+    { url: 'sflix.se', flags: ['en', 'movie', 'serie', '360p', '720p', '1080p', 'ads', 'web'] },
+    { url: 'bflixhd.to', flags: ['en', 'movie', 'serie', '360p', '720p', '1080p', 'ads', 'web'] },
+    { url: 'flixflare.to', flags: ['en', 'movie', 'serie', '360p', '720p', '1080p', 'ads', 'web'] },
+    { url: 'swatchseries.is', flags: ['en', '360p', 'ads', 'web'] },
+    { url: 'fmovies.to', flags: ['en', '360p', 'ads', 'web'] },
+    { url: 'filmoflix.to', flags: ['fr', 'vost-fr', '360p', 'ads', 'web'] },
+    { url: 'movies7.to', flags: ['en', '360p', 'ads', 'web'] },
+    { url: 'favmovies.to', flags: ['en', '360p', 'ads', 'web'] },
+    { url: 'yeahmovies.to', flags: ['en', '360p', 'ads', 'web'] },
+    { url: 'soap2dayx2.to', flags: ['en', '360p', 'ads', 'web'] },
+    { url: 'watchseriesx.to', flags: ['en', 'movie', 'serie', '360p', '720p', '1080p', 'ads', 'web'] },
+    { url: 'sflix.to', flags: ['en', '360p', 'ads', 'web'] },
+    { url: 'seriestreaming.in', flags: ['fr', '360p', 'ads', 'web'] },
+    { url: 'watch4freemovies.com', flags: ['360p', 'ads', 'web'] },
+    { url: 'soap2dayhd.co', flags: ['360p', 'ads', 'web'] },
+    { url: 'papadustream.bz', flags: ['360p', 'ads', 'web'] },
+    { url: 'soap2dayhd.com', flags: ['360p', 'ads', 'web'] },
+    { url: 'swatchseries.mx', flags: ['360p', 'ads', 'web'] },
+    { url: 'watchmovies.nz', flags: ['360p', 'ads', 'web'] },
+    { url: 'soap2day.ist', flags: ['360p', 'ads', 'web'] },
+    { url: 'soap2day.pe', flags: ['360p', 'ads', 'web'] },
+    { url: '123series.bz', flags: ['360p', 'ads', 'web'] },
+    { url: 'soap2day.tf', flags: ['360p', 'ads', 'web'] },
+    { url: 'ymovies.cc', flags: ['360p', 'ads', 'web'] },
+    { url: 'xmovies.ws', flags: ['360p', 'ads', 'web'] },
+    { url: 'watchseries.im', flags: ['360p', 'ads', 'web'] },
+    { url: 'watchseries.mn', flags: ['360p', 'ads', 'web'] },
+    { url: 'watchseries.pe', flags: ['360p', 'ads', 'web'] },
+    { url: 'watchserieshd.mx', flags: ['360p', 'ads', 'web'] },
+    { url: 'watchseriesstream.com', flags: ['360p', 'ads', 'web'] },
+    { url: '123moviesfree.net', flags: ['360p', 'ads', 'web'] },
+    { url: 'f2movies.to', flags: ['360p', 'ads', 'web'] },
+    { url: 'vipstream.tv', flags: ['360p', 'ads', 'web'] },
+    { url: 'upmovies.net', flags: ['360p', 'ads', 'web'] },
+    { url: '1movieshd.cc', flags: ['360p', 'ads', 'web'] },
+    { url: '1hd.to', flags: ['360p', 'ads', 'web'] },
+    { url: '1flix.to', flags: ['360p', 'ads', 'web'] },
+    { url: '123-movies.vc', flags: ['movie', 'serie', '360p', '720p', '1080p', 'ads', 'web'] },
+    { url: '123moviestv.net', flags: ['360p', 'ads', 'web'] }
 ];
 
 let currentIndex = 0;
+let flags = window.location.href.split('?').length > 1 ? window.location.href.split('?').pop().split('&') : ['web'];
+
+let sites = allSites.filter(site => flags.every(flag => site.flags.includes(flag)));
+
+console.log(sites);
 
 function updateSite() {
-    if (sites.length > 0) { // Vérifie que la liste des sites n'est pas vide
-        const site = sites[currentIndex];
-        document.body.style.backgroundImage = `url('${baseUrl + site.url + '.jpeg'}')`;
-    }
-}
 
+    let noneDisplay = [];
+
+    if (sites.length > 0) { // Vérifie que la liste des sites n'est pas vide
+        noneDisplay = ['notfound'];
+        
+        const site = sites[currentIndex];
+        document.body.style.backgroundImage = `url('${'./ressources/' + site.url + '.jpeg'}')`;
+    } else {
+        noneDisplay = ['playButton', 'prevButton', 'nextButton'];
+    }
+
+    noneDisplay.forEach(id => document.getElementById(id).style.display = 'none');
+}
 document.getElementById('playButton').onclick = () => {
     if (sites.length > 0) {
         window.location.href = 'https://' + sites[currentIndex].url;
@@ -66,6 +78,11 @@ document.getElementById('prevButton').onclick = () => {
 
 document.getElementById('nextButton').onclick = () => {
     currentIndex = (currentIndex + 1) % sites.length;
+    updateSite();
+};
+
+document.getElementById('back-home').onclick = () => {
+    window.location.href = window.location.href.split('?')[0];
     updateSite();
 };
 
